556193b76f083fb73c8355874d9897dbfc4f3c50
TrolledWoods <trolledwoods@gmail.com>
2023-12-04 07:00:22 +0100

    Optimized

--- rust_solutions/src/day4.rs
+++ rust_solutions/src/day4.rs
@@ -1,34 +1,96 @@
+use std::collections::VecDeque;
+
 pub fn part1(input: &str) -> i64 {
-    input.lines()
-        .map(|v| {
-            let (a, b) = v.split_once('|').unwrap();
-            let winnings = a.split_once(':').unwrap().1;
-            let winnings = winnings.split_whitespace().map(|v| v.trim()).collect::<Vec<_>>();
-            let count = b.split_whitespace().map(|v| v.trim()).filter(|v| winnings.contains(v)).count();
-            if count == 0 {
-                0
-            } else {
-                2_i64.pow(count as u32 - 1)
+    let mut input_set = 0_u128;
+    // For now we assume the inputs are not above 128, which is invalid!
+    // Something like this should be added as a fallback probably....
+    //
+    // let mut large_input_set = HashSet::default();
+    // let mut is_large = false;
+
+    let mut n = 0;
+    let mut count = 0;
+    let mut sum = 0;
+    let mut win_section = true;
+    for c in input.bytes().chain(Some(b'\n')) {
+        if c.is_ascii_digit() {
+            n = n*10 + (c-b'0') as u32;
+        } else {
+            if n != 0 {
+                if win_section {
+                    input_set |= 1 << n;
+                } else {
+                    if n < 128 && (input_set & (1 << n)) > 0 {
+                        count += 1;
+                    }
+                }
+
+                n = 0;
             }
-        })
-        .sum::<i64>()
+
+            if c == b':' {
+                win_section = true;
+            } else if c == b'|' {
+                win_section = false;
+            } else if c == b'\n' {
+                if count > 0 {
+                    sum += 2_i64.pow(count - 1);
+                }
+
+                count = 0;
+                input_set = 0;
+            }
+        }
+    }
+
+    sum
 }
 
 pub fn part2(input: &str) -> usize {
-    let mut copies = vec![];
-    input.lines()
-        .enumerate()
-        .map(|(i, v)| {
-            let (a, b) = v.split_once('|').unwrap();
-            let winnings = a.split_once(':').unwrap().1;
-            let winnings = winnings.split_whitespace().map(|v| v.trim()).collect::<Vec<_>>();
-            let count = b.split_whitespace().map(|v| v.trim()).filter(|v| winnings.contains(v)).count();
-            copies.resize_with(copies.len().max(i + count + 1), || 0);
-            let clones = copies[i] + 1;
-            copies[i+1..i+count+1].iter_mut().for_each(|v| *v += clones);
-            clones
-        })
-        .sum::<usize>()
+    let mut copies = VecDeque::new();
+    let mut input_set = 0_u128;
+    // For now we assume the inputs are not above 128, which is invalid!
+    // let mut large_input_set = HashSet::default();
+    // let mut is_large = false;
+
+    let mut n = 0;
+    let mut count = 0;
+    let mut sum = 0;
+    let mut win_section = true;
+    for c in input.bytes().chain(Some(b'\n')) {
+        if c.is_ascii_digit() {
+            n = n*10 + (c-b'0') as u32;
+        } else {
+            if win_section {
+                if n != 0 {
+                    input_set |= 1 << n;
+                }
+            } else {
+                if n != 0 && n < 128 && (input_set & (1 << n)) > 0 {
+                    count += 1;
+                }
+            }
+
+            if c == b':' {
+                win_section = true;
+            } else if c == b'|' {
+                win_section = false;
+            } else if c == b'\n' {
+                let n_cards = copies.pop_front().unwrap_or(0) + 1;
+                sum += n_cards;
+
+                copies.resize(copies.len().max(count), 0);
+                copies.range_mut(..count).for_each(|v| *v += n_cards);
+
+                count = 0;
+                input_set = 0;
+            }
+
+            n = 0;
+        }
+    }
+
+    sum
 }
 
 #[test]
