afe86a2e2b5104f79d1e063aece967aa26472883
TrolledWoods <trolledwoods@gmail.com>
2023-12-07 20:53:51 +0100

    Merged both parts of day 7

--- rust_solutions/src/day7.rs
+++ rust_solutions/src/day7.rs
@@ -1,37 +1,13 @@
 pub fn part1(input: &str) -> usize {
-    const NUM_CARDS: usize = 8 + 5;
-
-    let mut hands = Vec::with_capacity(input.len() / 8);
-    for (cards, bet) in input.lines().filter_map(|v| v.split_once(' ')) {
-        let mut card_counts = [0_u8; NUM_CARDS];
-        let mut card_value = 0;
-        for &card in &cards.as_bytes()[..5] {
-            let card = match card {
-                c @ b'2'..=b'9' => (c - b'2') as u32,
-                b'T' => 8,
-                b'J' => 9,
-                b'Q' => 10,
-                b'K' => 11,
-                b'A' => 12,
-                c => panic!("Invalid character {}", c as char),
-            };
-
-            card_counts[card as usize] += 1;
-            card_value = card_value * 16 + card;
-        }
-
-        let (max, next_max) = get_max_two(card_counts.into_iter());
-        let kind = (max) as u32 * 2 + (next_max == 2) as u32;
-        let bet = bet.parse::<u32>().unwrap();
-        hands.push(((kind << (4 * 5)) + card_value, bet));
-    }
-
-    hands.sort_unstable_by_key(|v| v.0);
-    hands.iter().enumerate().map(|(i, &(_, bet))| (i + 1) * bet as usize).sum()
+    solve(input, false)
 }
 
 pub fn part2(input: &str) -> usize {
-    const NUM_CARDS: usize = 8 + 4;
+    solve(input, true)
+}
+
+pub fn solve(input: &str, do_jokers: bool) -> usize {
+    const NUM_CARDS: usize = 13;
 
     let mut hands = Vec::with_capacity(input.len() / 8);
     for (cards, bet) in input.lines().filter_map(|v| v.split_once(' ')) {
@@ -40,22 +16,23 @@ pub fn part2(input: &str) -> usize {
         let mut card_value = 0;
         for &card in &cards.as_bytes()[..5] {
             let card = match card {
-                b'J' => 12,
-                c @ b'2'..=b'9' => (c - b'2') as u32,
-                b'T' => 8,
-                b'Q' => 9,
-                b'K' => 10,
-                b'A' => 11,
+                b'J' if do_jokers => 0,
+                c @ b'2'..=b'9' => (c - b'2' + 1) as u32,
+                b'T' => 9,
+                b'J' => 10,
+                b'Q' => 11,
+                b'K' => 12,
+                b'A' => 13,
                 c => panic!("Invalid character {}", c as char),
             };
 
-            if card == 12 {
+            if card == 0 {
                 n_jokers += 1;
-                card_value *= 16;
             } else {
-                card_counts[card as usize] += 1;
-                card_value = card_value * 16 + card + 1;
+                card_counts[card as usize - 1] += 1;
             }
+
+            card_value = card_value * 16 + card;
         }
 
         let (max, next_max) = get_max_two(card_counts.into_iter());
