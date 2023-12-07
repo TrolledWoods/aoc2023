a993470f0ddd15e7f99d76629163b20185244592
TrolledWoods <trolledwoods@gmail.com>
2023-12-07 13:54:55 +0100

    Simplified logic

--- rust_solutions/src/day7.rs
+++ rust_solutions/src/day7.rs
@@ -1,22 +1,11 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-enum HandKind {
-    HighCard,
-    OnePair,
-    TwoPair,
-    ThreeOfAKind,
-    FullHouse,
-    FourOfAKind,
-    FiveOfAKind,
-}
-
 pub fn part1(input: &str) -> usize {
     const NUM_CARDS: usize = 8 + 5;
 
-    let mut hands = Vec::new();
-    for (cards, bet) in input.lines().map(|v| v.trim()).filter_map(|v| v.split_once(' ')) {
+    let mut hands = Vec::with_capacity(input.len() / 8);
+    for (cards, bet) in input.lines().filter_map(|v| v.split_once(' ')) {
         let mut card_counts = [0_u8; NUM_CARDS];
         let mut card_value = 0;
-        for card in cards.bytes() {
+        for &card in &cards.as_bytes()[..5] {
             let card = match card {
                 c @ b'2'..=b'9' => (c - b'2') as u32,
                 b'T' => 8,
@@ -31,19 +20,10 @@ pub fn part1(input: &str) -> usize {
             card_value = card_value * 16 + card;
         }
 
-        card_counts.sort_unstable_by(|a, b| a.cmp(b).reverse());
-        let kind = match (card_counts[0], card_counts[1]) {
-            (5, _) => HandKind::FiveOfAKind,
-            (4, _) => HandKind::FourOfAKind,
-            (3, 2) => HandKind::FullHouse,
-            (3, _) => HandKind::ThreeOfAKind,
-            (2, 2) => HandKind::TwoPair,
-            (2, _) => HandKind::OnePair,
-            _ => HandKind::HighCard,
-        };
-
+        let (max, next_max) = get_max_two(card_counts.into_iter());
+        let kind = (max) as u32 * 2 + (next_max == 2) as u32;
         let bet = bet.parse::<u32>().unwrap();
-        hands.push((((kind as u32) << (4 * 5)) + card_value, bet));
+        hands.push(((kind << (4 * 5)) + card_value, bet));
     }
 
     hands.sort_unstable_by_key(|v| v.0);
@@ -53,12 +33,12 @@ pub fn part1(input: &str) -> usize {
 pub fn part2(input: &str) -> usize {
     const NUM_CARDS: usize = 8 + 4;
 
-    let mut hands = Vec::new();
-    for (cards, bet) in input.lines().map(|v| v.trim()).filter_map(|v| v.split_once(' ')) {
+    let mut hands = Vec::with_capacity(input.len() / 8);
+    for (cards, bet) in input.lines().filter_map(|v| v.split_once(' ')) {
         let mut n_jokers = 0;
         let mut card_counts = [0_u8; NUM_CARDS];
         let mut card_value = 0;
-        for card in cards.bytes() {
+        for &card in &cards.as_bytes()[..5] {
             let card = match card {
                 b'J' => 12,
                 c @ b'2'..=b'9' => (c - b'2') as u32,
@@ -78,23 +58,13 @@ pub fn part2(input: &str) -> usize {
             }
         }
 
-        card_counts.sort_unstable_by(|a, b| a.cmp(b).reverse());
-        let kind = match (card_counts[0] + n_jokers, card_counts[1]) {
-            (5, _) => HandKind::FiveOfAKind,
-            (4, _) => HandKind::FourOfAKind,
-            (3, 2) => HandKind::FullHouse,
-            (3, _) => HandKind::ThreeOfAKind,
-            (2, 2) => HandKind::TwoPair,
-            (2, _) => HandKind::OnePair,
-            _ => HandKind::HighCard,
-        };
-
+        let (max, next_max) = get_max_two(card_counts.into_iter());
+        let kind = (max + n_jokers) as u32 * 2 + (next_max == 2) as u32;
         let bet = bet.parse::<u32>().unwrap();
-        hands.push((((kind as u32) << (4 * 5)) + card_value, bet));
+        hands.push(((kind << (4 * 5)) + card_value, bet));
     }
 
     hands.sort_unstable_by_key(|v| v.0);
-
     hands.iter().enumerate().map(|(i, &(_, bet))| (i + 1) * bet as usize).sum()
 }
 
@@ -103,4 +73,19 @@ fn test_answers() {
     let input = std::fs::read_to_string("inputs/day7.txt").unwrap();
     assert_eq!(part1(&input), 248836197);
     assert_eq!(part2(&input), 251195607);
+}
+
+fn get_max_two<T: Ord + Default>(values: impl Iterator<Item = T>) -> (T, T) {
+    let mut max = T::default();
+    let mut next_max = T::default();
+
+    for value in values {
+        if value > max {
+            next_max = std::mem::replace(&mut max, value);
+        } else if value > next_max {
+            next_max = value;
+        }
+    }
+
+    (max, next_max)
 }
