a06b31324ad42432d57be74a4868352b3a3de2ae
TrolledWoods <trolledwoods@gmail.com>
2023-12-07 22:01:41 +0100

    Cleaned up day 7 solution more

--- rust_solutions/src/day7.rs
+++ rust_solutions/src/day7.rs
@@ -1,19 +1,20 @@
-pub fn part1(input: &str) -> usize {
+pub fn part1(input: &str) -> u64 {
     solve(input, false)
 }
 
-pub fn part2(input: &str) -> usize {
+pub fn part2(input: &str) -> u64 {
     solve(input, true)
 }
 
-pub fn solve(input: &str, do_jokers: bool) -> usize {
-    const NUM_CARDS: usize = 13;
+pub fn solve(input: &str, do_jokers: bool) -> u64 {
+    const NUM_CARDS: usize = 14;
 
-    let mut hands = Vec::with_capacity(input.len() / 8);
+    let mut hands = Vec::new();
     for (cards, bet) in input.lines().filter_map(|v| v.split_once(' ')) {
-        let mut n_jokers = 0;
+        let bet = bet.parse::<u32>().unwrap();
+
         let mut card_counts = [0_u8; NUM_CARDS];
-        let mut card_value = 0;
+        let mut cards_score = 0;
         for &card in &cards.as_bytes()[..5] {
             let card = match card {
                 b'J' if do_jokers => 0,
@@ -26,23 +27,28 @@ pub fn solve(input: &str, do_jokers: bool) -> usize {
                 c => panic!("Invalid character {}", c as char),
             };
 
-            if card == 0 {
-                n_jokers += 1;
-            } else {
-                card_counts[card as usize - 1] += 1;
-            }
-
-            card_value = card_value * 16 + card;
+            card_counts[card as usize] += 1;
+            cards_score = (cards_score << 4) + card;
         }
 
-        let (max, next_max) = get_max_two(card_counts.into_iter());
-        let kind = (max + n_jokers) as u32 * 2 + (next_max == 2) as u32;
-        let bet = bet.parse::<u32>().unwrap();
-        hands.push(((kind << (4 * 5)) + card_value, bet));
+        let n_jokers = card_counts[0];
+        let (max, next_max) = card_counts[1..].iter().fold(
+            (0, 0),
+            |(max, next_max), &v| if v > max {
+                (v, max)
+            } else if v > next_max {
+                (max, v)
+            } else {
+                (max, next_max)
+            }
+        );
+
+        let hand_score = (max + n_jokers) as u32 * 2 + (next_max == 2) as u32;
+        hands.push(((hand_score << 4 * 5) + cards_score, bet));
     }
 
-    hands.sort_unstable_by_key(|v| v.0);
-    hands.iter().enumerate().map(|(i, &(_, bet))| (i + 1) * bet as usize).sum()
+    hands.sort_unstable_by_key(|&(card, _)| card);
+    hands.iter().zip(1_u64..).map(|(&(_, bet), rank)| bet as u64 * rank).sum()
 }
 
 #[test]
@@ -50,19 +56,4 @@ fn test_answers() {
     let input = std::fs::read_to_string("inputs/day7.txt").unwrap();
     assert_eq!(part1(&input), 248836197);
     assert_eq!(part2(&input), 251195607);
-}
-
-fn get_max_two<T: Ord + Default>(values: impl Iterator<Item = T>) -> (T, T) {
-    let mut max = T::default();
-    let mut next_max = T::default();
-
-    for value in values {
-        if value > max {
-            next_max = std::mem::replace(&mut max, value);
-        } else if value > next_max {
-            next_max = value;
-        }
-    }
-
-    (max, next_max)
 }
